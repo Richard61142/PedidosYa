a7ccdcfccf1a347e3e1b1aed4d624a1fa62965ee
MoreiraC18 <150203868+MoreiraC18@users.noreply.github.com>
2024-01-28 18:08:35 -0500

    Update PedidosYaApp.py

--- PedidosYaApp.py
+++ PedidosYaApp.py
@@ -4,7 +4,7 @@ import mysql.connector
 connection = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="moreira520",
+    password="---------", #COLOCAR CONTRASEÑA DE LA BASE DA DATOS....
     database="pedidosya"
 )
 cursor = connection.cursor()
